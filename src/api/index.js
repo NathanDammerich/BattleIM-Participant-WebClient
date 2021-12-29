0a72a0ad7c25a41e35fee26dc798f83d8d6bb604
Nathan Dammerich <natedammerich@gmail.com>
2021-12-29 04:05:37 -0500

    Deployment

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://battleim-backend.herokuapp.com/",
+});
 
 export const getGames = () => API.get("/games");
 export const getGame = (id) => API.get(`/games/${id}`);
