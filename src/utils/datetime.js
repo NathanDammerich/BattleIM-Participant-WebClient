595e81a54fdc0be1a63089a86a3441b8894c7ad7
Nathan Dammerich <natedammerich@gmail.com>
2022-04-11 00:19:41 -0400

    time bug fixes

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -1,20 +1,21 @@
-import moment from 'moment';
+import moment from "moment";
 
 const getMonthDayString = (date) => {
-    if (date) {
-        return moment(date).format('MMMM d');
-    }
-    return 'TBA';
+  console.log(date);
+  if (date) {
+    moment.utc();
+    return moment(date).format("MMMM DD");
+  }
+  return "TBA";
 };
 const getTimeslotString = (timeslot) => {
-    if (Array.isArray(timeslot)) {
-        const currSlot = timeslot[0];
-        return `${currSlot.day} ${moment(currSlot.timeStart).format('h:mma')}-${moment(currSlot.timeEnd).format('h:mma')}`;
-    } 
-    return timeslot;
-}
+  if (Array.isArray(timeslot)) {
+    const currSlot = timeslot[0];
+    return `${currSlot.day} ${moment(currSlot.timeStart).format(
+      "h:mma"
+    )}-${moment(currSlot.timeEnd).format("h:mma")}`;
+  }
+  return timeslot;
+};
 
-export {
-    getMonthDayString,
-    getTimeslotString,
-}
+export { getMonthDayString, getTimeslotString };
